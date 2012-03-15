a3b837fb8a722be5d39000c53ac6fb27774e8abe
Marc Schröder <marc.schroeder@dfki.de>
2012-03-15 16:05:17 +0100

    Fixed and activated generic model add view test.

--- metashare/repo2/tests/test_editor.py
+++ metashare/repo2/tests/test_editor.py
@@ -7,6 +7,8 @@ from metashare.repo2 import models
 from django.contrib import admin
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.admin.sites import LOGIN_FORM_KEY
+from metashare.repo2.models import languageDescriptionInfoType_model,\
+    lexicalConceptualResourceInfoType_model
 
 ADMINROOT = '/{0}admin/'.format(DJANGO_BASE)
 
@@ -122,43 +124,44 @@ class EditorTest(TestCase):
         self.assertContains(response, 'Permission denied', status_code=403)
 
 
-    if False:
-        def test_can_see_models_add(self):
-            # Make sure admin.site.register() is actually executed:
-            # pylint: disable-msg=W0612
-            import metashare.repo2.admin
-            client = self.client_with_user_logged_in(self.editor_login)
-            items = models.__dict__.items()
-            num = 0
-            for key, value in items:
-            #print key
-            #print value
+    def test_editor_can_see_models_add(self):
+        # We don't expect the following add forms to work, because the editor
+        # always and exclusively uses the change form for these:
+        models_not_expected_to_work = (
+            languageDescriptionInfoType_model,
+            lexicalConceptualResourceInfoType_model,
+        )
+        
+        
+        # Make sure admin.site.register() is actually executed:
+        # pylint: disable-msg=W0612
+        import metashare.repo2.admin
+        client = self.client_with_user_logged_in(self.editor_login)
+        items = models.__dict__.items()
+        num = 0
+        for key, value in items:
+            if value in models_not_expected_to_work:
+                continue
             # Check only classes
-                if isinstance(value, type):
-                    #print type(value)
-                    #print 'issubclass = ' + unicode(issubclass(value, django.db.models.Model))
-                    # Check only subclasses of Django Model
-                    if issubclass(value, django.db.models.Model):
-                        cls_name = value.__name__
-                        print "class name = " + cls_name + ", module = " + value.__module__
-                        #mod = value.__module__
-                        #print "Module = " + mod
-                        # Avoid checking classes imported in metashare.repo2 from other modules
-                        #   like StorageObject
-                        if value.__module__.startswith('metashare.repo2'):
-                            # Check only registered models
-                            if value in admin.site._registry:
-                                #print 'Registered'
-                                model_url = key.lower()
-                                url = ADMINROOT+"repo2/{}/add/".format(model_url)
-                                print "Getting page {0} ...".format(url)
-                                response = client.get(url, follow=True)
-                                self.assertEquals(200, response.status_code)
-                                num = num + 1
-                                #print 'url = ' + url
+            if isinstance(value, type):
+                # Check only subclasses of Django Model
+                if issubclass(value, django.db.models.Model):
+                    cls_name = value.__name__
+                    # Avoid checking classes imported in metashare.repo2 from other modules
+                    #   like StorageObject
+                    if value.__module__.startswith('metashare.repo2'):
+                        # Check only registered models
+                        if value in admin.site._registry:
+                            model_url = key.lower()
+                            url = ADMINROOT+"repo2/{}/add/".format(model_url)
+                            print "For class {}, trying to access page {} ...".format(cls_name, url)
+                            response = client.get(url, follow=True)
+                            self.assertEquals(200, response.status_code)
+                            num = num + 1
+                        else:
+                            print 'Class {} has no registered admin form.'.format(cls_name)
                     print
-            print 'Checked models: {0}'.format(num)
-            self.assertTrue(True)
+        print 'Checked models: {0}'.format(num)
 
 
     if False:
