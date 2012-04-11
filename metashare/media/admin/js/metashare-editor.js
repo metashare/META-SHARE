6ad4a81b1949280f0422a96a79249d4531caa47d
Marc Schröder <marc.schroeder@dfki.de>
2012-04-11 15:12:19 +0200

    Added dropdown to share/create top-level editor menu to directly create Resource, Person, Organization, and Project.

--- metashare/media/admin/js/metashare-editor.js
+++ metashare/media/admin/js/metashare-editor.js
@@ -1,13 +1,13 @@
 // Any javascript required by the META-SHARE editor
 
 
-$(document).ready(function() {
+django.jQuery(document).ready(function() {
 
 	// Resource editor: content info stuff
   function compute_new_id(prefix) {
 	var counter = 0;
     while (true) {
-      var existing = $('#'+prefix+'-'+counter);
+      var existing = django.jQuery('#'+prefix+'-'+counter);
       if (existing.length === 0) break;
       counter++;
     }
@@ -17,10 +17,10 @@ $(document).ready(function() {
   function maybe_show_one_more_field(prefix) {
 	var new_id = compute_new_id(prefix);
 	var new_name = new_id.substr(3); // skip initial 'id_'
-	var inputItem = $('#'+new_id)
+	var inputItem = django.jQuery('#'+new_id)
 	if (inputItem.length === 0) {
 		// need to create it by cloning
-		var protodiv = $('#'+prefix+'-protodiv');
+		var protodiv = django.jQuery('#'+prefix+'-protodiv');
 		var newInput = protodiv.find('input').clone().attr({'id': new_id, 'name': new_name}).css('display', 'inline');
 		newInput.change(handle_input_update);
 		protodiv.before(newInput);
@@ -32,21 +32,21 @@ $(document).ready(function() {
   }
 
   function handle_input_update() {
-		var input_id = $(this).attr('id');
-		var input_value = $(this).attr('value');
-		var editA = $('#edit_' + input_id)
-		var addA = $('#add_' + input_id)
+		var input_id = django.jQuery(this).attr('id');
+		var input_value = django.jQuery(this).attr('value');
+		var editA = django.jQuery('#edit_' + input_id)
+		var addA = django.jQuery('#add_' + input_id)
 
-		if (input_id.localeCompare("has_value") == 0 && $('#'+input_id).attr('value')){
-			var temp = $('#'+input_id).attr('class');
-			$('a#'+temp).attr('class', '');
+		if (input_id.localeCompare("has_value") == 0 && django.jQuery('#'+input_id).attr('value')){
+			var temp = django.jQuery('#'+input_id).attr('class');
+			django.jQuery('a#'+temp).attr('class', '');
 		}		
 		if (input_value) { // edit
 			var protoHref = editA.attr('proto-href');
 			editA.attr('href', protoHref+input_value+'/').css('display', 'inline');
 			addA.css('display', 'none');
 			var prefix = input_id.substr(0, input_id.lastIndexOf('-'));
-			if ($('#'+prefix+'-protodiv').length > 0) {
+			if (django.jQuery('#'+prefix+'-protodiv').length > 0) {
 				maybe_show_one_more_field(prefix);
 			}
 		} else { // add
@@ -55,25 +55,50 @@ $(document).ready(function() {
 		}
 	  }
   
-  $('#contentInfoStuff input').change(handle_input_update);
+  django.jQuery('#contentInfoStuff input').change(handle_input_update);
   // end of Resource editor: content info stuff
   
 
  
 
   // Cancel button
-  $('input[name="_cancel"]').click(function() {
-	  var popup = $('input[name="_popup"][value="1"]');
+  django.jQuery('input[name="_cancel"]').click(function() {
+	  var popup = django.jQuery('input[name="_popup"][value="1"]');
 	  if (popup.length > 0) {
 		  window.close();
 	  } else {
 		  window.location = "../";
 	  }
   });
-
-  
-  
-  
   // End of Cancel button
 
 });
+
+
+
+// Drop-down menu
+var timeout         = 500;
+var closetimer		= 0;
+var ddmenuitem      = 0;
+
+function jsddm_open()
+{	jsddm_canceltimer();
+	jsddm_close();
+	ddmenuitem = django.jQuery(this).find('ul').eq(0).css('visibility', 'visible');}
+
+function jsddm_close()
+{	if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+function jsddm_timer()
+{	closetimer = window.setTimeout(jsddm_close, timeout);}
+
+function jsddm_canceltimer()
+{	if(closetimer)
+	{	window.clearTimeout(closetimer);
+		closetimer = null;}}
+
+django.jQuery(document).ready(function()
+{	django.jQuery('#jsddm > li').bind('mouseover', jsddm_open);
+	django.jQuery('#jsddm > li').bind('mouseout',  jsddm_timer);});
+
+document.onclick = jsddm_close;
