e6b658e83d3822c64c85548d4b305da5bccce298
Marc Schröder <marc.schroeder@dfki.de>
2012-04-20 09:02:25 +0200

    One more fix repo2->repository, to keep generator code working.

--- misc/tools/generateMeta/generateMeta.py
+++ misc/tools/generateMeta/generateMeta.py
@@ -351,7 +351,7 @@ def main():
     NoQuestions = False
     showVersion = False
     xschemaFileName = None
-    package_prefix = "metashare.repo2."
+    package_prefix = "metashare.repository."
     DEBUG = False
     ForceOptionalChoices = False
     
