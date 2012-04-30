5ca21fe585f2b449abcbfcd132b9a437b78e89de
Marc Schröder <marc.schroeder@dfki.de>
2012-04-30 14:02:44 +0200

    Automatic verification for consistency between DJANGO_URL and DJANGO_BASE upon startup (issue #113)

--- metashare/repository/__init__.py
+++ metashare/repository/__init__.py
@@ -14,6 +14,7 @@ def verify_at_startup():
     """
     
     check_solr_running()
+    check_settings()
 
 
 def check_solr_running():
@@ -31,3 +32,21 @@ Caused by: {}
 **************************************************************************
 """.format(solr_error)
         raise Exception(_msg)
+
+def check_settings():
+    def fail(msg):
+        raise Exception(u'Error in settings: {}'.format(msg))
+    from metashare.settings import DJANGO_BASE, DJANGO_URL
+    if DJANGO_URL.endswith('/'):
+        fail('DJANGO_URL must not end in a slash')
+    if not (DJANGO_BASE.endswith('/') or DJANGO_BASE == ''):
+        fail('DJANGO_BASE must end in a slash or be the empty string')
+    # from base, remove the trailing slash:
+    base_path = DJANGO_BASE[:-1]
+    # From url, remove the protocol if present:
+    url_path = '://' in DJANGO_URL and DJANGO_URL[DJANGO_URL.find('://')+3:] or DJANGO_URL
+    # and then the host/port:
+    url_path = '/' in url_path and url_path[url_path.find('/')+1:] or ''
+    if base_path != url_path:
+        fail(u"Based on DJANGO_URL '{}', expected DJANGO_BASE '{}/', but was '{}'".format(DJANGO_URL, url_path, DJANGO_BASE))
+    
