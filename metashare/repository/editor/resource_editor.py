b1c003c22e7630b07859ca7df6f08d0e90eccd1c
Salvatore Minutoli <salvatore.minutoli@iit.cnr.it>
2012-05-16 14:32:23 +0200

    Use help icon instead of text as (Show help) or (More)

--- metashare/repository/editor/resource_editor.py
+++ metashare/repository/editor/resource_editor.py
@@ -556,6 +556,7 @@ class ResourceModelAdmin(SchemaModelAdmin):
             'save_on_top': self.save_on_top,
             'kb_link': settings.KNOWLEDGE_BASE_URL,
             'comp_name': _('%s') % force_unicode(opts.verbose_name),
+            'help_icon_url': u'%s%s' % (settings.MEDIA_URL, "css/sexybuttons/images/icons/silk/help.png"),
         }
         if extra_context:
             context.update(extra_context)
