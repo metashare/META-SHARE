07130bdc5c0a43f4177082cee85f61d5c6be73f5
dmavroeidis <dmavroeidis@ilsp.gr>
2012-05-25 11:31:24 +0300

    Renewed comments for 'required' functionality in single resource view

--- metashare/repository/supermodel.py
+++ metashare/repository/supermodel.py
@@ -492,15 +492,19 @@ class SchemaModel(models.Model):
                             _element.text = SchemaModel._python_to_xml(
                                                                 _sub_value[1])                            
                             if self.is_required_field(_model_field):
-                                
-                                _element.required = self.is_required_field(_model_field)
+								# If the element is "required" in the model,
+								# turn the "required" value of the element
+								# tree to "1" (true), otherwise "0" (false).
+                                _element.required = self.is_required_field(
+                                                                _model_field)
                             else:
                                 _element.required = 0                            
                         else:
                             _element.text = SchemaModel._python_to_xml(
                                                                 _sub_value)                            
                             if self.is_required_field(_model_field):
-                                _element.required = self.is_required_field(_model_field)
+                                _element.required = self.is_required_field(
+                                                                _model_field)
                             else:
                                 _element.required = 0
                         _current_node.append(_element)
