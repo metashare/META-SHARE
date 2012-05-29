852b2272f44702ff12b7d4fe4b0787ed349ca357
Christian Spurk <cspurk@dfki.de>
2012-05-29 15:04:20 +0200

    tidy up imports

--- metashare/repository/tests/test_model.py
+++ metashare/repository/tests/test_model.py
@@ -1,12 +1,17 @@
 import sys
-from xml.etree.ElementTree import fromstring, tostring, register_namespace
-from django.test import TestCase
-from metashare import test_utils
-from metashare.settings import ROOT_PATH
-from metashare.repository.models import resourceInfoType_model, SCHEMA_NAMESPACE, \
-    lingualityInfoType_model
-from metashare.xml_utils import pretty_xml
+
 from difflib import unified_diff
+
+from django.test import TestCase
+
+from xml.etree.ElementTree import fromstring, tostring, register_namespace
+
+from metashare import test_utils
+from metashare.repository.models import resourceInfoType_model, \
+    SCHEMA_NAMESPACE, lingualityInfoType_model
+from metashare.settings import ROOT_PATH
+from metashare.xml_utils import pretty_xml
+
 
 class ModelTest(TestCase):
     """
