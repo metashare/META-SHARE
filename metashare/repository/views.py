e44ac21041e22139607befbc41fbf42f9f9c9139
Olivier Hamon <hamon@elda.org>
2012-06-11 15:59:35 +0200

    Add redirection for licence/download

--- metashare/repository/views.py
+++ metashare/repository/views.py
@@ -193,6 +193,12 @@ def download(request, object_id):
     resource = get_object_or_404(resourceInfoType_model, storage_object__identifier=object_id)
     licences = _get_licences(resource, request.user)
 
+    # Check whether the resource is from the current node, or whether it must be redirected to the master copy
+    url = ''
+    if isinstance(resource, resourceInfoType_model):
+        if (not resource.storage_object.master_copy):
+            url = "{0}{1}".format(resource.storage_object.source_url, resource.get_absolute_url())
+
     licence_choice = None
     if request.method == "POST":
         licence_choice = request.POST.get('licence', None)
@@ -206,7 +212,8 @@ def download(request, object_id):
                     { 'form': la_form, 'resource': resource,
                       'licence_path': \
                       LICENCEINFOTYPE_URLS_LICENCE_CHOICES[licence_choice][0],
-                      'download_available': licences[licence_choice][1] },
+                      'download_available': licences[licence_choice][1],
+                      'redirection_url': url },
                     context_instance=RequestContext(request))
         elif licence_choice and not licence_choice in licences:
             licence_choice = None
@@ -221,11 +228,13 @@ def download(request, object_id):
               'resource': resource,
               'licence_path': \
                 LICENCEINFOTYPE_URLS_LICENCE_CHOICES[licence_choice][0],
-              'download_available': licences[licence_choice][1] },
+              'download_available': licences[licence_choice][1],
+              'redirection_url': url },
             context_instance=RequestContext(request))
     elif len(licences) > 1:
         return render_to_response('repository/licence_selection.html',
-            { 'form': LicenseSelectionForm(licences), 'resource': resource },
+            { 'form': LicenseSelectionForm(licences), 'resource': resource,
+             'redirection_url': url },
             context_instance=RequestContext(request))
     else:
         return render_to_response('repository/lr_not_downloadable.html',
