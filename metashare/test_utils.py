27af9530c00fb1999e319b7320ab6bdd14b18c03
Christian Spurk <cspurk@dfki.de>
2012-06-11 15:18:13 +0200

    create a utility methods for easily creating new editor/manager users

--- metashare/test_utils.py
+++ metashare/test_utils.py
@@ -4,11 +4,12 @@ Utility functions for unit tests useful across apps.
 """
 import os
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User, Permission
 from django.core.management import call_command
 from django.test.testcases import TestCase
 
 from metashare import settings
+from metashare.repository.management import GROUP_GLOBAL_EDITORS
 from metashare.repository.models import resourceInfoType_model
 from metashare.storage.models import PUBLISHED
 from metashare.xml_utils import import_from_file
@@ -38,6 +39,36 @@ def import_xml(filename):
 def import_xml_or_zip(filename):
     _xml = open(filename, 'rb')
     return import_from_file(_xml, filename, PUBLISHED)
+
+
+def create_manager_user(user_name, email, password, groups=None):
+    """
+    Creates a new managing editor user account with the given credentials and
+    group memberships.
+    """
+    result = create_editor_user(user_name, email, password, groups)
+    # add resource delete permission
+    opts = resourceInfoType_model._meta
+    result.user_permissions.add(Permission.objects.filter(
+        content_type__app_label=opts.app_label,
+        codename=opts.get_delete_permission())[0])
+    return result
+
+
+def create_editor_user(user_name, email, password, groups=None):
+    """
+    Creates a new editor user account with the given credentials and group
+    memberships.
+    """
+    result = User.objects.create_user(user_name, email, password)
+    result.is_staff = True
+    if groups:
+        for group in groups:
+            result.groups.add(group)
+    # always add basic editing permissions
+    result.groups.add(Group.objects.get(name=GROUP_GLOBAL_EDITORS))
+    result.save()
+    return result
 
 
 class IndexAwareTestCase(TestCase):
