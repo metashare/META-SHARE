12be5b5f27a0bc6a8a84fa0aa460867334f7944f
dmavroeidis <dmavroeidis@ilsp.gr>
2012-06-18 17:11:32 +0300

    Finished script to run synchronization in predefined intervals.

--- metashare/sync/cron.py
+++ metashare/sync/cron.py
@@ -1,8 +1,14 @@
 import kronos
-#import os, sys
+from metashare.settings import INTERVALS
 from django.core.management import call_command
 
-@kronos.register('0 0 * * *')
+interval_settings = ""
+# Get time interval settings
+for item in INTERVALS.values():
+    interval_settings += " " + str(item)
+interval_settings = interval_settings.strip()
+
+@kronos.register(interval_settings)
 def run_synchronization():
     call_command('synchronize', interactive=False)
     
