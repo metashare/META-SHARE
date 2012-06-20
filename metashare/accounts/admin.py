0de8dd3e1021730fa64799519334c37e3a741396
Olivier Hamon <hamon@elda.org>
2012-06-20 11:27:32 +0200

    Pylint fixes

--- metashare/accounts/admin.py
+++ metashare/accounts/admin.py
@@ -3,6 +3,7 @@ Project: META-SHARE prototype implementation
  Author: Christian Federmann <cfedermann@dfki.de>
 """
 from django import forms
+from django.forms import Form
 from django.contrib import admin
 from django.contrib.admin.options import csrf_protect_m
 from django.contrib.auth.models import Permission, Group
@@ -48,15 +49,15 @@ class EditorGroupAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     actions = ('add_user_profile_to_editor_group', 'remove_user_profile_from_editor_group', )
 
-    class AddUserProfiletoEditorGroupForm(forms.Form):
-            _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
-            
-            def __init__(self, choices = None, *args, **kwargs):
-                super(forms.Form, self).__init__(*args, **kwargs)
-                if choices is not None:
-                    self.choices = choices
-                    self.fields['user profiles'] = forms.ModelMultipleChoiceField(self.choices)
-    
+    class UserProfileinEditorGroupForm(Form):
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+
+        def __init__(self, choices = None, *args, **kwargs):
+            super(Form, self).__init__(*args, **kwargs)
+            if choices is not None:
+                self.choices = choices
+                self.fields['user profiles'] = forms.ModelMultipleChoiceField(self.choices)
+
     @csrf_protect_m
     @transaction.commit_on_success
     def add_view(self, request, form_url='', extra_context=None):
@@ -77,42 +78,48 @@ class EditorGroupAdmin(admin.ModelAdmin):
                                 form_url=form_url, extra_context=extra_context)
 
     def queryset(self, request):
-        qs = super(EditorGroupAdmin, self).queryset(request)
+        queryset = super(EditorGroupAdmin, self).queryset(request)
 
         if request.user.is_superuser:
-            return qs
+            return queryset
 
         user_qs = UserProfile.objects.filter(user=request.user)
         mg_user = user_qs[0].managergroup.all()
-        return qs.filter(managergroup__in=mg_user)
+        return queryset.filter(managergroup__in=mg_user)
 
     def add_user_profile_to_editor_group(self, request, queryset):
-            form = None
-            if 'cancel' in request.POST:
-                self.message_user(request, 'Cancelled adding user profiles to the editor group.')
-                return
-            elif 'add_user_profile_to_editor_group' in request.POST:
-                q = UserProfile.objects.all()
-                form = self.AddUserProfiletoEditorGroupForm(q, request.POST)
-                if form.is_valid():
-                    userprofiles = form.cleaned_data['user profiles']
-                    for userprofile in userprofiles:
-                        for obj in queryset:
-                            if UserProfile.objects.filter(user=request.user)[0].has_editor_group_add_user_permission(obj):
-                                userprofile.editorgroup.add(obj)
-                            else:
-                                self.message_user(request, 'You need to be group manager to add a user to this editor group.')
-                                return HttpResponseRedirect(request.get_full_path())
-                    self.message_user(request, 'Successfully added user profiles to editor group.')
-                    return HttpResponseRedirect(request.get_full_path())
-    
-            if not form:
-               userprofiles = UserProfile.objects.all()
-               form = self.AddUserProfiletoEditorGroupForm(choices=userprofiles, initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
-            
-            return render_to_response('accounts/add_user_profile_to_editor_group.html', \
-                                      {'selected_editorgroups': queryset, 'form': form, 'path':request.get_full_path()}, \
-                                      context_instance=RequestContext(request))
+        form = None
+        if 'cancel' in request.POST:
+            self.message_user(request, 'Cancelled adding user profiles to the editor group.')
+            return
+        elif 'add_user_profile_to_editor_group' in request.POST:
+            objs_up = UserProfile.objects.all()
+            form = self.UserProfileinEditorGroupForm(objs_up, request.POST)
+            if form.is_valid():
+                userprofiles = form.cleaned_data['user profiles']
+                for userprofile in userprofiles:
+                    for obj in queryset:
+                        if UserProfile.objects.filter(user=request.user)[0].has_editor_group_add_user_permission(obj):
+                            userprofile.editorgroup.add(obj)
+                        else:
+                            self.message_user(request,
+                                'You need to be group manager to add a user to this editor group.')
+                            return HttpResponseRedirect(request.get_full_path())
+                self.message_user(request, 'Successfully added user profiles to editor group.')
+                return HttpResponseRedirect(request.get_full_path())
+
+        if not form:
+            userprofiles = UserProfile.objects.all()
+            form = self.UserProfileinEditorGroupForm(choices=userprofiles,
+                initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
+        
+        return render_to_response('accounts/add_user_profile_to_editor_group.html',
+                                  {
+                                   'selected_editorgroups': queryset,
+                                   'form': form,
+                                   'path':request.get_full_path()
+                                  },
+                                  context_instance=RequestContext(request))
 
     add_user_profile_to_editor_group.short_description = "Add user profiles to selected editor groups"
 
@@ -123,8 +130,8 @@ class EditorGroupAdmin(admin.ModelAdmin):
                 self.message_user(request, 'Cancelled removing user profiles from the editor group.')
                 return
             elif 'remove_user_profile_from_editor_group' in request.POST:
-                q = UserProfile.objects.all()
-                form = self.AddUserProfiletoEditorGroupForm(q, request.POST)
+                objs_up = UserProfile.objects.all()
+                form = self.UserProfileinEditorGroupForm(objs_up, request.POST)
                 if form.is_valid():
                     userprofiles = form.cleaned_data['user profiles']
                     for userprofile in userprofiles:
@@ -132,17 +139,23 @@ class EditorGroupAdmin(admin.ModelAdmin):
                             if UserProfile.objects.filter(user=request.user)[0].has_editor_group_add_user_permission(obj):
                                 userprofile.editorgroup.remove(obj)
                             else:
-                                self.message_user(request, 'You need to be group manager to remove a user from this editor group.')
+                                self.message_user(request,
+                                    'You need to be group manager to remove a user from this editor group.')
                                 return HttpResponseRedirect(request.get_full_path())
                     self.message_user(request, 'Successfully remove user profiles from editor group.')
                     return HttpResponseRedirect(request.get_full_path())
     
             if not form:
                 userprofiles = UserProfile.objects.all()                    
-                form = self.AddUserProfiletoEditorGroupForm(choices=userprofiles, initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
+                form = self.UserProfileinEditorGroupForm(choices=userprofiles,
+                    initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
         
-            return render_to_response('accounts/remove_user_profile_from_editor_group.html', \
-                                      {'selected_editorgroups': queryset, 'form': form, 'path':request.get_full_path()}, \
+            return render_to_response('accounts/remove_user_profile_from_editor_group.html',
+                                      {
+                                       'selected_editorgroups': queryset,
+                                       'form': form,
+                                       'path':request.get_full_path()
+                                      },
                                       context_instance=RequestContext(request))
 
     remove_user_profile_from_editor_group.short_description = "Remove user profiles from selected editor groups"
