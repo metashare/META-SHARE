c2deaf4dcf6e40e658dfcbd5a37490f017af2dc8
jsteffen <steffen@dfki.de>
2012-06-27 12:41:41 +0200

    added unit tests for uniqueness of user name and email address in account registration

--- metashare/accounts/tests.py
+++ metashare/accounts/tests.py
@@ -9,6 +9,8 @@ from metashare.accounts.models import RegistrationRequest
 from django.core.exceptions import ValidationError
 from django.test.client import Client
 from metashare.settings import DJANGO_BASE
+from django.db.utils import IntegrityError
+from django.contrib.auth.models import User
 
 class CreateViewTest(django.test.TestCase):
     def testCreateInitial(self):
@@ -42,19 +44,22 @@ class CreateViewTest(django.test.TestCase):
         self.assertContains(response, "Please fill in all fields",
           status_code=200)
 
+    def tearDown(self):
+        RegistrationRequest.objects.all().delete()
+        User.objects.all().delete()
 
-class RegistrationRequestTest(unittest.TestCase):
+class RegistrationRequestTest(django.test.TestCase):
 
     def setUp(self):
         self.reg_request = RegistrationRequest.objects.create(
           shortname='test', firstname='Test', lastname='Testson',
-          email='test@b.com')
+          email='test@test.com')
         self.broken_request1 = RegistrationRequest.objects.create(
-          firstname='Test', lastname='Testson', email='broken1@b.com')
+          firstname='Test', lastname='Testson', email='broken1@test.com')
         self.broken_request2 = RegistrationRequest.objects.create(
-          shortname='broken2',  lastname='Testson', email='broken2@b.com')
+          shortname='broken2',  lastname='Testson', email='broken2@test.com')
         self.broken_request3 = RegistrationRequest.objects.create(
-          shortname='broken3', firstname='Test',  email='broken3@b.com')
+          shortname='broken3', firstname='Test',  email='broken3@test.com')
         self.broken_request4 = RegistrationRequest.objects.create(
           shortname='broken4', firstname='Test', lastname='Testson',
           email='not an email')
@@ -67,7 +72,7 @@ class RegistrationRequestTest(unittest.TestCase):
         self.assertEqual('test', self.reg_request.shortname)
         self.assertEqual('Test', self.reg_request.firstname)
         self.assertEqual('Testson', self.reg_request.lastname)
-        self.assertEqual('test@b.com', self.reg_request.email)
+        self.assertEqual('test@test.com', self.reg_request.email)
 
     def testCanRetrieveFromDB(self):
         test_entry = RegistrationRequest.objects.get(pk=self.reg_request.pk)
@@ -117,14 +122,54 @@ class RegistrationRequestTest(unittest.TestCase):
         except ValidationError:
             pass
 
+    def testUniqueUserNameRegistration(self):
+        try:
+            self.reg_request = RegistrationRequest.objects.create(
+              shortname=self.reg_request.shortname,
+              firstname='Test', lastname='Testson',
+              email='test2@test2.com')
+            self.fail("Should have thrown an IntegrityError due to duplicate " \
+              "user name")
+        except IntegrityError:
+            pass
+    
+    def testUniqueEmailRegistration(self):
+        try:
+            self.reg_request = RegistrationRequest.objects.create(
+              shortname='test2', firstname='Test', lastname='Testson',
+              email=self.reg_request.email)
+            self.fail("Should have thrown an IntegrityError due to duplicate " \
+              "email")
+        except IntegrityError:
+            pass
 
+    def testUniqueUserNameAccount(self):
+        User.objects.create_user(
+            self.reg_request.shortname, self.reg_request.email, 'secret')
+        client = Client()
+        post_data = {'shortname':self.reg_request.shortname, 'firstname':'Test',
+          'lastname':'Testson', 'email':'test2@test2.com'}
+        response = client.post('/{0}accounts/create/'.format(DJANGO_BASE),
+          follow=True, data=post_data)
+        self.assertEqual('accounts/create_account.html', response.templates[0].name)
+        self.assertContains(response, "User name already exists, please choose another one.",
+          status_code=200)
+          
+    def testUniqueEmailAccount(self):
+        User.objects.create_user(
+            self.reg_request.shortname, self.reg_request.email, 'secret')
+        client = Client()
+        post_data = {'shortname':'test2', 'firstname':'Test',
+          'lastname':'Testson', 'email':self.reg_request.email}
+        response = client.post('/{0}accounts/create/'.format(DJANGO_BASE),
+          follow=True, data=post_data)
+        self.assertEqual('accounts/create_account.html', response.templates[0].name)
+        self.assertContains(response, "There is already an account registered with this email.",
+          status_code=200)
+       
     def tearDown(self):
-        self.reg_request.delete()
-        self.broken_request1.delete()
-        self.broken_request2.delete()
-        self.broken_request3.delete()
-        self.broken_request4.delete()
-        self.broken_request5.delete()
+        RegistrationRequest.objects.all().delete()
+        User.objects.all().delete()
 
 if __name__ == "__main__":
     unittest.main()
