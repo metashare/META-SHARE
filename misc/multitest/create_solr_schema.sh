9eb9db5a048f0258cf29f946edec15862d00f00d
Salvatore Minutoli <salvatore.minutoli@iit.cnr.it>
2012-07-03 19:30:58 +0200

    Automatically create SOLR schema for multinode configuration. Update resource names to match existing files.

--- misc/multitest/create_solr_schema.sh
+++ misc/multitest/create_solr_schema.sh
@@ -5,10 +5,21 @@ CURRENT_DIR=`pwd`
 METASHARE_DIR=$METASHARE_SW_DIR/metashare
 SCHEMA_FILE=$CURRENT_DIR/init_data/schema.xml
 
+cp init_data/settings_orig.py ../../metashare/settings.py
+cp init_data/local_settings_test.py ../../metashare/local_settings.py
+
 export DJANGO_PORT=12345
 export STORAGE_PATH=/tmp/storageFolder
 export SOLR_PORT=54321
 export DATABASE_FILE=$CURRENT_DIR/init_data/metashare_test.db
+
+# find the best Python binary to use: only use the platform default if ther is
+# no custom Python installation for META-SHARE available
+if [ -x "$METASHARE_SW_DIR/opt/bin/python" ] ; then
+	PYTHON="$METASHARE_SW_DIR/opt/bin/python"
+else
+	PYTHON=`which python`
+fi
 
 cd $METASHARE_DIR
 python manage.py build_solr_schema --filename="$SCHEMA_FILE"
