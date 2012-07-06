7ee7913998334a9fd120fd16865ed8e5fc980dac
jsteffen <steffen@dfki.de>
2012-07-06 16:15:40 +0200

    added logging

--- metashare/sync/management/commands/check_proxied_nodes.py
+++ metashare/sync/management/commands/check_proxied_nodes.py
@@ -5,11 +5,12 @@ from django.core.management.base import BaseCommand
 from metashare import settings
 from metashare.storage.models import PROXY, StorageObject, RemovedObject
 from metashare.sync.sync_utils import remove_resource
+import sys
 
 class Command(BaseCommand):
     
-    help = 'Checks for each proxy resource if its source node is still in the' \
-      + 'in the proxied node list; if not removes the resource'
+    help = 'Checks for each proxy resource if its source node is still in the ' \
+      + 'proxied node list; if not removes the resource'
     
     def handle(self, *args, **options):
         
@@ -20,12 +21,15 @@ class Command(BaseCommand):
 
         # iterate over proxy resources and check for each if its source url
         # is still listed in the proxied node list
+        remove_count = 0
         for proxy_res in StorageObject.objects.filter(copy_status=PROXY):
             if not proxy_res.source_url in proxy_urls:
                 # delete the associated resource and create a RemovedObject
                 # to let the other nodes know that the resource has been removed
                 # when synchronizing
+                sys.stdout.write("\nremoving proxied resource {}\n".format(proxy_res.identifier))
+                remove_count += 1
                 rem_obj = RemovedObject.objects.create(identifier=proxy_res.identifier)
                 rem_obj.save()
                 remove_resource(proxy_res)
-
+        sys.stdout.write("\n{} proxied resources removed\n".format(remove_count))
