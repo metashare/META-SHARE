d698064ff4aef759416b94464bd5c4bceae66ec1
Salvatore Minutoli <salvatore.minutoli@iit.cnr.it>
2012-07-11 17:13:30 +0200

    Removed settings for METASHARE_SW_DIR and TEST_DIR in setvars.sh

--- misc/tools/multitest/setvars.sh
+++ misc/tools/multitest/setvars.sh
@@ -1,6 +1,6 @@
 
-export METASHARE_SW_DIR=/home/metashare/current/META-SHARE
-export TEST_DIR=/tmp
+#export METASHARE_SW_DIR=/home/metashare/current/META-SHARE
+#export TEST_DIR=/tmp
 
 REPORT_DIR="$METASHARE_SW_DIR/metashare/reports"
 
