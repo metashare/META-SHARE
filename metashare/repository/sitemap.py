0ccfc7dcd477a447a734dc8afbf174aca1d850bc
dmavroeidis <dmavroeidis@ilsp.gr>
2012-07-17 14:26:30 +0300

    Exclude unpublished resources from the sitemap.

--- metashare/repository/sitemap.py
+++ metashare/repository/sitemap.py
@@ -1,14 +1,15 @@
 from django.contrib.sitemaps import Sitemap
-from repository.models import resourceInfoType_model
-from settings import DJANGO_FULL_URL
+from metashare.repository.models import resourceInfoType_model
+from metashare.settings import DJANGO_FULL_URL
+from metashare.storage.models import PUBLISHED
 
 class RepositorySitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
     
     def items(self):
-        return resourceInfoType_model.objects.all()
-    
+        return resourceInfoType_model.objects.filter(storage_object__publication_status=PUBLISHED).filter(storage_object__deleted=False)
+
     def location(self, obj):
         url = '{}/{}'.format(DJANGO_FULL_URL, obj.get_relative_url())
         # Specific to sitemap module. It insists on prepending a
