7860ee49f442672d7d69a6c1eb6c3878cd6545b2
dmavroeidis <dmavroeidis@ilsp.gr>
2012-07-18 11:26:41 +0300

    Changes in how the sitemap is produced.

--- metashare/repository/sitemap.py
+++ metashare/repository/sitemap.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from metashare.repository.models import resourceInfoType_model
-from metashare.settings import DJANGO_FULL_URL
+from metashare.settings import DJANGO_URL
 from metashare.storage.models import PUBLISHED
 
 class RepositorySitemap(Sitemap):
@@ -11,7 +11,7 @@ class RepositorySitemap(Sitemap):
         return resourceInfoType_model.objects.filter(storage_object__publication_status=PUBLISHED).filter(storage_object__deleted=False)
 
     def location(self, obj):
-        url = '{}/{}'.format(DJANGO_FULL_URL, obj.get_relative_url())
+        url = '{}{}'.format(DJANGO_URL, obj.get_absolute_url())
         # Specific to sitemap module. It insists on prepending a
         # "http://" before the url.
         if url.startswith("http://"):
