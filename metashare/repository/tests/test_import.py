5e120c50c21ca1c856d6e50e89dd42a5925a8778
Olivier Hamon <hamon@elda.org>
2012-07-24 15:25:07 +0200

    Add two unit tests for imported LR metadata with default editor group of the user

--- metashare/repository/tests/test_import.py
+++ metashare/repository/tests/test_import.py
@@ -4,16 +4,28 @@ from metashare import test_utils
 from metashare.settings import ROOT_PATH
 from metashare.repository.models import documentUnstructuredString_model, \
     documentInfoType_model
+from metashare.accounts.models import EditorGroup
+from django.test.client import Client
+from metashare.settings import DJANGO_BASE, ROOT_PATH
 
 class ImportTest(TestCase):
     """
     Tests the import procedure for resources
     """
+
+    test_editor_group = None
+    editor_user = None
     
     @classmethod
     def setUpClass(cls):
         test_utils.set_index_active(False)
-    
+
+        ImportTest.test_editor_group = EditorGroup.objects.create(
+                                                    name='test_editor_group')
+
+        ImportTest.editor_user = test_utils.create_editor_user('editoruser',
+            'editor@example.com', 'secret', (ImportTest.test_editor_group,))
+   
     @classmethod
     def tearDownClass(cls):
         test_utils.set_index_active(True)
@@ -95,3 +107,32 @@ class ImportTest(TestCase):
         # and 1 documentInfo in the database
         self.assertEqual(len(documentUnstructuredString_model.objects.all()), 2)
         self.assertEqual(len(documentInfoType_model.objects.all()), 1)
+
+    def test_imported_resource_get_user_default_editor_group(self):
+        """
+        Check if resource editor group is set to the default editor group of the user.
+        """
+        client = Client()
+        client.login(username='editoruser', password='secret')
+
+        ImportTest.editor_user.get_profile().default_editor_groups \
+            .add(ImportTest.test_editor_group)
+
+        resourcefile = open('{}/repository/fixtures/testfixture.xml'.format(ROOT_PATH))
+        response = client.post('/{0}/upload_xml/'.format(DJANGO_BASE), \
+          {'resource': resourcefile}, follow=True)
+        self.assertNotContains(response, '<td>{}</td>'.format(ImportTest.test_editor_group.name),
+          msg_prefix='expected the system to set an editor group to the resource.')
+
+    def test_imported_resource_get_none_if_no_default_editor_group(self):
+        """
+        Check if resource editor group is set to None if the user do not have a default editor group.
+        """
+        client = Client()
+        client.login(username='editoruser', password='secret')
+
+        resourcefile = open('{}/repository/fixtures/testfixture.xml'.format(ROOT_PATH))
+        response = client.post('/{0}/upload_xml/'.format(DJANGO_BASE), \
+          {'resource': resourcefile}, follow=True)
+        self.assertNotContains(response, '<td>{}</td>'.format(ImportTest.test_editor_group.name),
+          msg_prefix='expected the system to set None as editor group to the resource.')
