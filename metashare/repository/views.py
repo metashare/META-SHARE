e36e34588bb47558cb959ff9f812e6276d4b7eca
Christian Spurk <cspurk@dfki.de>
2012-07-24 10:33:54 +0200

    various improvements of the recommendations block
    * only show recommendation title, if there are any recommendations
    * refactor duplicate code into a function
    * use constants instead of hard-coded strings
    * tidy up imports
    * use the node-local URL, where possible

--- metashare/repository/views.py
+++ metashare/repository/views.py
@@ -24,16 +24,18 @@ from haystack.views import FacetedSearchView
 
 from metashare.repository.editor.resource_editor import has_edit_permission
 from metashare.repository.forms import LicenseSelectionForm, \
-    LicenseAgreementForm, DownloadContactForm
+    LicenseAgreementForm, DownloadContactForm, MORE_FROM_SAME_CREATORS, \
+    MORE_FROM_SAME_PROJECTS
 from metashare.repository.models import licenceInfoType_model, resourceInfoType_model
 from metashare.repository.search_indexes import resourceInfoType_modelIndex
 from metashare.settings import LOG_LEVEL, LOG_HANDLER, MEDIA_URL, DJANGO_URL
 from metashare.stats.model_utils import getLRStats, saveLRStats, \
     saveQueryStats, VIEW_STAT, DOWNLOAD_STAT
 from metashare.storage.models import PUBLISHED
-from metashare.recommendations.recommendations import SessionResourcesTracker
-from metashare.recommendations.recommendations import Resource
 from metashare.recommendations.models import TogetherManager
+from metashare.recommendations.recommendations import SessionResourcesTracker, \
+    Resource
+
 
 MAXIMUM_READ_BLOCK_SIZE = 4096
 
@@ -452,39 +454,36 @@ def view(request, resource_name=None, object_id=None):
         tracker.add_view(resource, datetime.now())
         request.session['tracker'] = tracker
             
-    # Add 'also viewed' resources
-    also_viewed_resources = []
-    man = TogetherManager.getManager(Resource.VIEW)
-    view_res = man.getTogetherList(resource, 0)
-    for res in view_res:
-        res_item = {}
-        res_item['name'] = res.__unicode__()
-        res_item['url'] = "{0}/{1}".format(res.storage_object.source_url,
-                               res.get_relative_url())
-        also_viewed_resources.append(res_item)
-    context['also_viewed'] = also_viewed_resources
-    
-    # Add 'also downloaded' resources
-    also_downloaded_resources = []
-    man = TogetherManager.getManager(Resource.DOWNLOAD)
-    down_res = man.getTogetherList(resource, 0)
-    for res in down_res:
-        res_item = {}
-        res_item['name'] = res.__unicode__()
-        res_item['url'] = "{0}/{1}".format(res.storage_object.source_url,
-                               res.get_relative_url())
-        also_downloaded_resources.append(res_item)
-    context['also_downloaded'] = also_downloaded_resources
-    
-    context['search_rel_projects'] = '{0}/repository/search?q=mfsp:{1}'.format(DJANGO_URL,
-                                            resource.storage_object.identifier)
-    
-    context['search_rel_creators'] = '{0}/repository/search?q=mfsc:{1}'.format(DJANGO_URL,
-                                            resource.storage_object.identifier)
-    
+    # Add recommendations for 'also viewed' resources
+    context['also_viewed'] = \
+        _get_together_recommendations(resource, Resource.VIEW)
+    # Add recommendations for 'also downloaded' resources
+    context['also_downloaded'] = \
+        _get_together_recommendations(resource, Resource.DOWNLOAD)
+    # Add 'more from same' links
+    context['search_rel_projects'] = '{}/repository/search?q={}:{}'.format(
+        DJANGO_URL, MORE_FROM_SAME_PROJECTS, resource.storage_object.identifier)
+    context['search_rel_creators'] = '{}/repository/search?q={}:{}'.format(
+        DJANGO_URL, MORE_FROM_SAME_CREATORS, resource.storage_object.identifier)
+
     # Render and return template with the defined context.
     ctx = RequestContext(request)
     return render_to_response(template, context, context_instance=ctx)
+
+
+def _get_together_recommendations(resource, together_type):
+    '''
+    Returns a list of recommendations of the given `Resource` type for the given
+    resource (as used in the single resource view).
+    '''
+    result = []
+    for res in TogetherManager.getManager(together_type) \
+            .getTogetherList(resource, 0):
+        res_item = {}
+        res_item['name'] = res.__unicode__()
+        res_item['url'] = res.get_absolute_url()
+        result.append(res_item)
+        return result
 
 
 class MetashareFacetedSearchView(FacetedSearchView):
