8afdd109e4a69daa89eb39605fecc95503b5ea19
Christian Spurk <cspurk@dfki.de>
2012-09-06 12:39:52 +0200

    a sync user’s password may change in SYNC_USERS so always update the DB

--- metashare/sync/management/__init__.py
+++ metashare/sync/management/__init__.py
@@ -1,7 +1,15 @@
+import logging
+
 from django.db.models import get_models, signals
 from metashare.sync import models as sync_models
 from metashare import settings
 from django.contrib.auth.models import User
+
+
+# Setup logging support.
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(settings.LOG_HANDLER)
+
 
 def create_syncuser(app, created_models, verbosity, **kwargs):
     from django.core.management import call_command
@@ -10,7 +18,13 @@ def create_syncuser(app, created_models, verbosity, **kwargs):
     
     for username, password in syncusers.iteritems():
         try:
-            User.objects.get(username=username)
+            _user = User.objects.get(username=username)
+            # the password may have been changed in the SYNC_USERS dict, so make
+            # sure to always update it in the database
+            _user.set_password(password)
+            LOGGER.info("Reset password for sync user account %s to the "
+                        "SYNC_USERS settings.", username)
+            _user.save()
         except User.DoesNotExist:
             call_command("createsyncuser", username=username, password=password, verbosity=1)
 
