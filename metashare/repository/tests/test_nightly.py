7dca7ae8544e478072e1aefcbffc64c60ed3f4c4
jsteffen <steffen@dfki.de>
2012-09-07 21:07:39 +0200

    first version of extended single resource view test with checks for all content found in the exported XML

--- metashare/repository/tests/test_nightly.py
+++ metashare/repository/tests/test_nightly.py
@@ -1,17 +1,18 @@
 import logging
+from zipfile import ZipFile
 from django.test import TestCase
+from django.test.client import Client
+from django.utils.encoding import smart_str
 from metashare import test_utils, xml_utils
 from metashare.settings import ROOT_PATH, LOG_HANDLER, TEST_MODE_NAME
 from metashare.repository.supermodel import OBJECT_XML_CACHE
 from metashare.repository.models import resourceInfoType_model
-from zipfile import ZipFile
-from django.test.client import Client
 
 # Setup logging support.
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(LOG_HANDLER)
 
-RESOURCES_ZIP_FILE = '{0}/../misc/testdata/v2.1/metashare_resources_v2.1.zip'.format(ROOT_PATH) 
+RESOURCES_ZIP_FILE = '{0}/../misc/testdata/v2.1/metashare_resources_v2.zip'.format(ROOT_PATH) 
 
 class NightlyTests(TestCase):
     """
@@ -76,8 +77,9 @@ class NightlyTests(TestCase):
                     res_count += 1
         # check that every xml file is available as resource
         self.assertEqual(len(resourceInfoType_model.objects.all()), res_count)
-    
-    
+
+
+    #TODO replace with next text when single resource view is complete
     def testSingleResourceView(self):
         """
         Checks that each resource's single view is displayed correctly.
@@ -99,3 +101,67 @@ class NightlyTests(TestCase):
 
         # enable indexing 
         test_utils.set_index_active(True)
+
+
+    #TODO activate when single resource view is complete
+    def deactivated_testSingleResourceViewAll(self):
+        """
+        Checks that each resource's single view is displayed correctly.
+        """
+        
+        # disable indexing; we don't need stat updates for this test
+        test_utils.set_index_active(False)
+        
+        count = 0
+        error_atts = []
+        for _res in resourceInfoType_model.objects.all():
+            parent_dict = {}
+            _res.export_to_elementtree(pretty=True, parent_dict=parent_dict)       
+
+            count += 1
+            LOGGER.info("calling {}. resource at {}".format(
+              count, _res.get_absolute_url()))
+            # always create a new client to force a new session
+            client = Client()
+            response = client.get(_res.get_absolute_url(), follow = True)
+            self.assertEquals(200, response.status_code)
+            self.assertTemplateUsed(response, 'repository/lr_view.html')
+            for _ele in parent_dict:
+                if not _ele.text:
+                    continue
+                text = smart_str(xml_utils.html_escape(_ele.text), response._charset)
+                real_count = response.content.count(text)
+                if real_count == 0:
+                    path = self.path_to_root(_ele, parent_dict)
+                    if "email" in path \
+                      or "metaShareId" in path:
+                        continue
+                    LOGGER.error(u"missing {}: {}".format(path, _ele.text))
+                    error_atts.append(path)
+                # TODO activate when single resource view is complete
+                #self.assertContains(response, xml_utils.html_escape(_ele.text))
+                
+        print "missing paths:"
+        for path in sorted(set(error_atts)):
+            print path
+        # enable indexing 
+        test_utils.set_index_active(True)
+        
+        
+    def path_to_root(self, element, parent_dict):
+        """
+        Returns the path to the given element using the given parent dictionary.
+        """
+        current = element
+        ele_path = []
+        ele_path.append(element.tag)
+        while current in parent_dict:
+            parent = parent_dict[current] 
+            ele_path.append(parent.tag)
+            current = parent
+        ele_path.reverse()
+        path = ""
+        for ele in ele_path:
+            path += ele
+            path += "/"
+        return path[:-1]
