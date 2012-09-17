a98a78e1f744ae6ca9de708054d92a2c6da7811e
jsteffen <steffen@dfki.de>
2012-09-17 17:43:36 +0200

    added remove_node command

---
+++ metashare/sync/management/commands/remove_node.py
@@ -0,0 +1,35 @@
+"""
+Management utility to handle removed nodes.
+"""
+from django.core.management.base import BaseCommand
+from metashare import settings
+from metashare.storage.models import PROXY, StorageObject, RemovedObject
+from metashare.sync.sync_utils import remove_resource
+import logging
+
+# Setup logging support.
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(settings.LOG_HANDLER)
+
+class Command(BaseCommand):
+    
+    args = '<node_name node_name ...>'
+    help = 'Removes all resources of the nodes with the given names; ' \
+      + 'if node is a proxied node, propagates the removal'
+    
+    def handle(self, *args, **options):
+        
+        for node_name in args:
+            LOGGER.info("checking node {}".format(node_name))
+            remove_count = 0
+            for res in StorageObject.objects.filter(source_node=node_name):
+                remove_count += 1
+                LOGGER.info("removing resource {}".format(res.identifier))
+                if res.copy_status == PROXY:
+                    rem_obj = RemovedObject.objects.create(identifier=res.identifier)
+                    rem_obj.save()
+                    LOGGER.info("creating RemovedObject for resource {}".format(
+                      res.identifier))
+                remove_resource(res)
+            LOGGER.info("removed {} resources of node {}".format(remove_count, node_name))
+
