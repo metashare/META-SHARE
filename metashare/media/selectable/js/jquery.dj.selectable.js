2d4eb837433a671d10a1e784ca1584e25561a0cf
Salvatore Minutoli <salvatore.minutoli@iit.cnr.it>
2012-09-19 13:39:10 +0200

    Clear the input field after an item is selected from an autocomplete list.

--- metashare/media/selectable/js/jquery.dj.selectable.js
+++ metashare/media/selectable/js/jquery.dj.selectable.js
@@ -171,6 +171,12 @@
                         });
                         $(input).after(newInput);
                         self._addDeckItem(newInput);
+                        /* Clear the input text again after some time since
+                         * some other event handler tries to set the selected value
+                         */
+                        setTimeout(function(){
+                        	$(input).val("");
+                        }, 200);
                         return false;
                     }
                 } else {
