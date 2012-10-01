2fb2c77af06d79ab8dd7195adacd611050b202af
jsteffen <steffen@dfki.de>
2012-10-01 21:09:27 +0200

    fine tuned unit tests for full resource view

--- metashare/repository/tests/test_view.py
+++ metashare/repository/tests/test_view.py
@@ -16,8 +16,11 @@ from metashare.settings import DJANGO_BASE, ROOT_PATH, LOG_HANDLER, \
 from metashare.test_utils import create_user
 from metashare.repository.supermodel import OBJECT_XML_CACHE
 from metashare.repository.models import resourceInfoType_model
-from metashare.repository.tests.test_nightly import path_to_root
 from django.utils.encoding import smart_str
+from django.contrib.humanize.templatetags import humanize
+from metashare.utils import prettify_camel_case_string
+from django.utils.formats import date_format
+from datetime import datetime
 
 # Setup logging support.
 LOGGER = logging.getLogger(__name__)
@@ -794,7 +797,7 @@ class FullViewTest(TestCase):
                 "full-corpus-textngram.xml".format(ROOT_PATH))
         test_utils.import_xml_or_zip("{}/repository/fixtures/full-resources/"
                 "full-corpus-textnumerical.xml".format(ROOT_PATH))
-
+                
         # enable indexing 
         test_utils.set_index_active(True)
     
@@ -833,38 +836,122 @@ class FullViewTest(TestCase):
         # disable indexing; we don't need stat updates for this test
         test_utils.set_index_active(False)
         
-        count = 0
-        error_atts = []
-        for _res in resourceInfoType_model.objects.all():
-            parent_dict = {}
-            _res.export_to_elementtree(pretty=True, parent_dict=parent_dict)       
+        queryset = resourceInfoType_model.objects.all()
+        check_resource_view(queryset, self)
 
-            count += 1
-            LOGGER.info("calling {}. resource at {}".format(
-              count, _res.get_absolute_url()))
-            # always create a new client to force a new session
-            client = Client()
-            response = client.get(_res.get_absolute_url(), follow = True)
-            self.assertEquals(200, response.status_code)
-            self.assertTemplateUsed(response, 'repository/resource_view/lr_view.html')
-            for _ele in parent_dict:
-                if not _ele.text:
-                    continue
-                text = smart_str(xml_utils.html_escape(_ele.text), response._charset)
-                real_count = response.content.count(text)
-                if real_count == 0:
-                    path = path_to_root(_ele, parent_dict)
-                    if "email" in path \
-                      or "metaShareId" in path:
-                        continue
-                    LOGGER.error(u"missing {}: {}".format(path, _ele.text))
-                    error_atts.append(path)
-                # TODO activate when single resource view is complete
-                #self.assertContains(response, xml_utils.html_escape(_ele.text))
-
-        if LOGGER.isEnabledFor(logging.WARN):
-            LOGGER.warn("missing paths:")
-            for path in sorted(set(error_atts)):
-                LOGGER.warn(path)
         # enable indexing 
         test_utils.set_index_active(True)
+
+
+def path_to_root(element, parent_dict):
+    """
+    Returns the path to the given element using the given parent dictionary.
+    """
+    current = element
+    ele_path = []
+    ele_path.append(element.tag)
+    while current in parent_dict:
+        parent = parent_dict[current] 
+        ele_path.append(parent.tag)
+        current = parent
+    ele_path.reverse()
+    path = ""
+    for ele in ele_path:
+        path += ele
+        path += "/"
+    return path[:-1]
+    
+
+def check_resource_view(queryset, test_case):
+    
+    # paths elemenets for which the path is skipped
+    skip_path_elements = (
+      'email',
+      'metaShareId',
+    )
+    
+    # path suffixes where to apply a number transformation on the value
+    number_paths = (
+      '/size',
+      '/fee',
+    )
+    
+    # path suffixes where to apply data transformation on the value
+    date_paths = (
+      '/metadataCreationDate',
+      '/annotationStartDate',
+      '/annotationEndDate',
+      '/availabilityStartDate',
+      '/availabilityEndDate',
+      '/creationStartDate',
+      '/creationEndDate',
+      '/projectStartDate',
+      '/projectEndDate',
+      'lastDateUpdated',
+    )
+    
+    count = 0
+    error_atts = []
+    for _res in queryset:
+        parent_dict = {}
+        _res.export_to_elementtree(pretty=True, parent_dict=parent_dict)       
+
+        count += 1
+        LOGGER.info("calling {}. resource at {}".format(
+          count, _res.get_absolute_url()))
+        # always create a new client to force a new session
+        client = Client()
+        response = client.get(_res.get_absolute_url(), follow = True)
+        test_case.assertEquals(200, response.status_code)
+        test_case.assertTemplateUsed(response, 'repository/resource_view/lr_view.html')
+        
+        for _ele in parent_dict:
+        
+            if not _ele.text:
+                continue
+        
+            path = path_to_root(_ele, parent_dict)
+            text = smart_str(xml_utils.html_escape(_ele.text), response._charset)
+                            
+            # check if path should be skipped
+            skip = False
+            for path_ele in skip_path_elements:
+                if path_ele in path:
+                    skip = True
+                    break
+            if skip:
+                continue    
+
+            # apply number transformation if required
+            for _np in number_paths:        
+                if path.endswith(_np):
+                    text = unicode(humanize.intcomma(text)).encode("utf-8")
+                    if text == '0':
+                        skip = True
+                    break
+            if skip:
+                continue
+                
+            # apply date transformation if required
+            for _dp in date_paths:
+                if path.endswith(_dp):
+                    date_object = datetime.strptime(text, '%Y-%m-%d')
+                    text = unicode(
+                      date_format(date_object, format='SHORT_DATE_FORMAT', use_l10n=True)).encode("utf-8")
+
+            real_count = response.content.count(text)
+            if real_count == 0:
+                # try with beautified string
+                beauty_real_count = response.content.count(
+                  prettify_camel_case_string(text))
+            if real_count == 0 and beauty_real_count == 0:
+                LOGGER.error(u"missing {}: {}".format(path, _ele.text))
+                error_atts.append(path)
+            # TODO activate when single resource view is complete
+            #test_case.assertContains(response, xml_utils.html_escape(_ele.text))
+
+    if LOGGER.isEnabledFor(logging.WARN):
+        LOGGER.warn("missing paths:")
+        for path in sorted(set(error_atts)):
+            LOGGER.warn(path)
+    
