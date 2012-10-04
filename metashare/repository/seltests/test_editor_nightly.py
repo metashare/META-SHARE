6235239d768c80de2b5ee5750192befdeae6b42c
Olivier Hamon <hamon@elda.org>
2012-10-04 17:53:12 +0200

    Refactor LR Language Description Text creation

--- metashare/repository/seltests/test_editor_nightly.py
+++ metashare/repository/seltests/test_editor_nightly.py
@@ -13,7 +13,8 @@ from metashare.repository.seltests.test_editor import _delete, _publish, \
     _fill_audioSize_form, _add_new_resource, _fill_administrativeInformation_forms, \
     _fill_corpusTextInfo_popup, _fill_corpusAudioInfo_popup, _fill_corpusVideoInfo_popup, \
     _fill_corpusImageInfo_popup, _fill_corpusTextNumericalInfo_popup, \
-    _fill_corpusTextNgramInfo_popup
+    _fill_corpusTextNgramInfo_popup, _fill_corpusLanguageDescriptionGeneralInfo_popup, \
+    _fill_corpusLanguageDescriptionTextInfo_popup
 from metashare.repository.seltests.test_utils import login_user, mouse_over, \
     setup_screenshots_folder, click_menu_item, save_and_close, \
     cancel_and_close, cancel_and_continue
@@ -449,59 +450,22 @@ class NightlyEditorTests(SeleniumTestCase):
         self.assertEqual("Logout", 
           driver.find_element_by_xpath("//div[@id='inner']/div[2]/a/div").text)
         driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        # Manage Resources -> Manage all resources
-        mouse_over(driver, driver.find_element_by_link_text("Manage Resources"))
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        click_menu_item(driver, driver.find_element_by_link_text("Manage all resources"))
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        # Add resource
-        driver.find_element_by_link_text("Add Resource").click()
-        # create language description
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        Select(driver.find_element_by_id("id_resourceType")).select_by_visible_text(
-          "Language description")
-        driver.find_element_by_id("id_submit").click()
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
+        # new resource with specific resource type and media types
+        _add_new_resource(driver, ss_path, "Language description", [])
         self.assertEqual("Add Resource", 
           driver.find_element_by_css_selector("#content > h1").text)
         # remember root window id
         root_id = driver.current_window_handle
-        # add required fields
-        driver.find_element_by_name("key_form-0-resourceName_0").clear()
-        driver.find_element_by_name("key_form-0-resourceName_0").send_keys("en")
-        driver.find_element_by_name("val_form-0-resourceName_0").clear()
-        driver.find_element_by_name("val_form-0-resourceName_0").send_keys("Test Text Language Description")
-        driver.find_element_by_name("key_form-0-description_0").clear()
-        driver.find_element_by_name("key_form-0-description_0").send_keys("en")
-        driver.find_element_by_name("val_form-0-description_0").clear()
-        driver.find_element_by_name("val_form-0-description_0").send_keys("Test Description")
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        # distribution popup
-        driver.find_element_by_css_selector("img[alt=\"Add information\"]").click()
-        _fill_distribution_popup(driver, ss_path, root_id)
-        # contact person popup
-        driver.find_element_by_css_selector("img[alt=\"Add Another\"]").click()
-        _fill_contactPerson_popup(driver, ss_path, root_id)
+        # administrative information
+        _fill_administrativeInformation_forms(driver, ss_path, root_id)
 
         # language description general info popup
         driver.find_element_by_id("edit_id_langdescInfo").click()
-        driver.switch_to_window("edit_id_langdescInfo")
-        Select(driver.find_element_by_id("id_languageDescriptionType")).select_by_visible_text(
-          "Grammar")
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time())) 
-        save_and_close(driver, root_id)
+        _fill_corpusLanguageDescriptionGeneralInfo_popup(driver, ss_path, root_id)
 
-        # language description info text popup
+        # language description text info popup
         driver.find_element_by_id("add_id_languageDescriptionTextInfo").click()
-        driver.switch_to_window("id_languageDescriptionTextInfo")
-        Select(driver.find_element_by_id("id_form-2-0-lingualityType")).select_by_visible_text(
-          "Monolingual")
-        # language description info text / language
-        driver.find_element_by_css_selector("img[alt=\"Add Another\"]").click()
-        _fill_language_form(driver, ss_path, "languageinfotype_model_set-0-")
-        # save and close language description info text popup
-        driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        save_and_close(driver, root_id)
+        _fill_corpusLanguageDescriptionTextInfo_popup(driver, ss_path, root_id)
 
         # save language description text
         driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
@@ -509,7 +473,7 @@ class NightlyEditorTests(SeleniumTestCase):
         # TODO remove this workaround when Selenium starts working again as intended
         time.sleep(1)
         driver.get_screenshot_as_file('{0}/{1}.png'.format(ss_path, time.time()))
-        self.assertEqual("The Resource \"Test Text Language Description\" was added successfully.", 
+        self.assertEqual("The Resource \"Resource Name\" was added successfully.", 
           driver.find_element_by_css_selector("li.info").text)
 
         # check the editor group of the resource is the default editor group of the user
