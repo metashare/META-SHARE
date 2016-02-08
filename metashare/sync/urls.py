56a6b257e557aec647ef1a21c08b9a6823e99ad4
lliu <lin@elda.org>
2016-02-08 15:15:32 +0100

    remove the sync app

--- metashare/sync/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-
-urlpatterns = patterns('metashare.sync.views',
-  (r'^$', 'inventory'),
-  (r'^(?P<resource_uuid>[0-9a-fA-F]{64})/metadata/$', 'full_metadata'),
-)
