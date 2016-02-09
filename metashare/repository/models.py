702d35034cd79e03937fd3fbc347fff20a915266
Miltos Deligiannis <mdeligiannis@outlook.com>
2016-02-09 14:24:06 +0200

    update enumerations / toolservice subtype free text

--- metashare/repository/models.py
+++ metashare/repository/models.py
@@ -3719,7 +3719,7 @@ ACTUALUSEINFOTYPE_USENLPSPECIFIC_CHOICES = _make_choices_from_list([
   u'textToSpeechSynthesis',u'textualEntailment', u'tokenization',
   u'tokenizationAndSentenceSplitting',u'topicDetection_Tracking',
   u'userAuthentication',u'visualSceneUnderstanding', u'voiceControl',
-  u'wordAlignment',u'wordSenseDisambiguation',
+  u'wordAlignment',u'wordSenseDisambiguation', u'lexiconMerging'
 ])
 
 # pylint: disable-msg=C0103
@@ -4124,18 +4124,20 @@ class durationOfAudioInfoType_model(SchemaModel):
         return self.unicode_(formatstring, formatargs)
 
 AUDIOFORMATINFOTYPE_MIMETYPE_CHOICES = _make_choices_from_list([
-  u'text/plain', u'application/vnd.xmi+xml', u'application/xml',
+  u'text/plain', u'application/vnd.xmi+xml', u'text/xml',
   u'application/x-tmx+xml',u'application/x-xces+xml',
   u'application/tei+xml',u'application/rdf+xml', u'application/xhtml+xml',
   u'application/emma+xml',u'application/pls+xml',
   u'application/voicexml+xml',u'text/sgml', u'text/html',
   u'application/x-tex',u'application/rtf', u'application/x-latex',
   u'text/csv',u'text/tab-separated-values', u'application/pdf',
-  u'application/x-msaccess',u'audio/mp4', u'audio/mpeg', u'audio/x-wav',
+  u'application/x-msaccess',u'audio/mp4', u'audio/mpeg', u'audio/wav',
   u'image/bmp',u'image/gif', u'image/jpeg', u'image/png', u'image/svg+xml',
   u'image/tiff',u'video/jpeg', u'video/mp4', u'video/mpeg', u'video/x-flv',
   u'video/x-msvideo',u'video/x-ms-wmv', u'application/msword',
-  u'application/vnd.ms-excel',u'audio/mpeg3', u'text/turtle', u'other',
+  u'application/vnd.ms-excel',u'audio/mpeg3', u'text/turtle',
+  u'audio/flac', u'audio/PCMA', u'audio/speex', u'audio/vorbis', u'video/mp2t',
+  u'other',
 ])
 
 AUDIOFORMATINFOTYPE_SIGNALENCODING_CHOICES = _make_choices_from_list([
@@ -7549,13 +7551,10 @@ class toolServiceInfoType_model(resourceComponentTypeType_model):
                      key=lambda choice: choice[1].lower()),
       )
 
-    toolServiceSubtype = MultiSelectField(
+    toolServiceSubtype = MultiTextField(max_length=100, widget=MultiFieldWidget(widget_id=63, max_length=100),
       verbose_name='Subtype of tool / service',
       help_text='Specifies the subtype of tool or service',
-      blank=True,
-      max_length=1 + len(TOOLSERVICEINFOTYPE_TOOLSERVICESUBTYPE_CHOICES['choices']) / 4,
-      choices=TOOLSERVICEINFOTYPE_TOOLSERVICESUBTYPE_CHOICES['choices'],
-      )
+      blank=True, null=True, validators=[validate_matches_xml_char_production], )
 
     languageDependent = MetaBooleanField(
       verbose_name='Language dependent',
