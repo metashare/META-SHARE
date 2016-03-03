627c75173c8e17c52edcb3316e0c9855e94ce3cc
JuliBakagianni <jbakagianni@gmail.com>
2016-03-03 17:19:31 +0200

    south doesnt recognize model properties. check by field value

--- metashare/repository/migrations/__init__.py
+++ metashare/repository/migrations/__init__.py
@@ -1,4 +1,5 @@
 from metashare.repository.model_utils import get_root_resources
+from metashare.storage.models import MASTER
 
 def write_to_resources_to_be_modified_file(curation_file, component, value, field_name):
     """
@@ -11,7 +12,7 @@ def write_to_resources_to_be_modified_file(curation_file, component, value, fiel
     """
     resources = get_root_resources(component)
     master_resources = [res.identificationInfo.get_default_resourceName() for res in resources \
-        if res.storage_object.master_copy and not res.storage_object.deleted]
+        if res.storage_object.copy_status==MASTER and not res.storage_object.deleted]
     if len(master_resources) > 0:
         resources_str = u', '.join(master_resources)
         curation_file.write(u'{}: {} in resources: {} \n' \
