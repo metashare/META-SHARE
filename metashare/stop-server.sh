703e38fdfa58487eb36f4c4b8413af301cdf482e
lliu <lin@elda.org>
2016-04-28 17:19:24 +0200

    merge to 3.1

--- metashare/stop-server.sh
+++ metashare/stop-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # META-SHARE Django website
 
-PROJECT_ROOT=`pwd`
+PROJECT_ROOT=$(dirname "$0")
 DJANGO_PID="$PROJECT_ROOT/django.pid"
 LIGHTTPD_PID="$PROJECT_ROOT/lighttpd.pid"
 
@@ -15,8 +15,11 @@ if [ -f $LIGHTTPD_PID ]; then
     rm -f $LIGHTTPD_PID
 fi
 
-# Unregister scheduled tasks for synchronization, session cleanup, etc.
-python2.7 manage.py uninstalltasks
+source "${PROJECT_ROOT}/../venv/bin/activate"
 
+# Unregister scheduled tasks for synchronization, session cleanup, etc.
+(cd "$PROJECT_ROOT/.." && python2.7 manage.py uninstalltasks )
+
+deactivate
 
 $PROJECT_ROOT/stop-solr.sh
