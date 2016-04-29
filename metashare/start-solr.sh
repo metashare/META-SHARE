cb3fc0faea8a0a111b60c304489775a1a1dc5db0
lliu <lin@elda.org>
2016-04-29 12:04:01 +0200

    merge to 3.1

--- metashare/start-solr.sh
+++ metashare/start-solr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export METASHAREDIR=`pwd`
+export METASHARETOPDIR=$(readlink -f $(dirname "$0")/..)
+export METASHAREDIR=$(dirname "$0")
 export SOLR_ROOT=$(cd "$METASHAREDIR/../solr" ; pwd)
 export SOLR_LOG=$SOLR_ROOT/solr.log
-export SOLR_PORT=8984
+export SOLR_PORT=8983
 export SOLR_STOP_PORT=8079
 export SOLR_STOP_KEY=stopkey
 
@@ -14,16 +15,22 @@ export TESTINGSCHEMAFILE=$SOLR_ROOT/solr/testing/conf/schema.xml
 echo "Checking for a previous running SOLR server..."
 "$METASHAREDIR/stop-solr.sh"
 
+sleep 5
 
 # Update schema.xml files, just in case:
-python2.7 $METASHAREDIR/manage.py build_solr_schema --filename="$MAINSCHEMAFILE"
-cp "$MAINSCHEMAFILE" "$TESTINGSCHEMAFILE" 
+source "${METASHAREDIR}/../venv/bin/activate"
+
+python "$METASHARETOPDIR/manage.py" build_solr_schema --filename="$MAINSCHEMAFILE"
+
+deactivate
+
+cp "$MAINSCHEMAFILE" "$TESTINGSCHEMAFILE"
 
 echo "Trying to start SOLR server"
 
 # Actually start solr
 (cd "$SOLR_ROOT"
-nohup java -Djetty.port=$SOLR_PORT -DSTOP.PORT=$SOLR_STOP_PORT -DSTOP.KEY="$SOLR_STOP_KEY" -jar start.jar > "$SOLR_LOG" &
+nohup java -Djetty.port=$SOLR_PORT -DSTOP.PORT=$SOLR_STOP_PORT -DSTOP.KEY="$SOLR_STOP_KEY" -jar start.jar > "$SOLR_LOG" 2>&1 &
 )
 
 
