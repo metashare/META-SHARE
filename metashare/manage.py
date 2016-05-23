ab016e5113d14150913ed214942cd2bbb87113fa
lliu <lin@elda.org>
2016-05-23 15:02:48 +0200

    remove useless codes

--- metashare/manage.py
+++ metashare/manage.py
@@ -25,16 +25,7 @@ except ImportError:
 
 if __name__ == "__main__":
     # MS, 21.03.2012: Add a fail-early verification "hook"
-    """
-    fail_early_commands = ('runserver', 'runfcgi')
-    if len(sys.argv) > 1:
-        command = sys.argv[1]
-        if command in fail_early_commands:
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-            from metashare.repository import verify_at_startup
-            verify_at_startup() # may raise Exception, which we don't want to catch.
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    """
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     
     from django.core.management import execute_from_command_line
