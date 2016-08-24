eb383b9bf2159b344f0cd735b764e9b9473f0fdd
MiltosD <mdeligiannis@outlook.com>
2016-08-24 11:53:33 +0300

    modify sync views to handle buggy behaviour in python 2.7.9

--- metashare/sync/views.py
+++ metashare/sync/views.py
@@ -1,3 +1,4 @@
+from StringIO import StringIO
 from django.http import HttpResponse
 import json
 from zipfile import ZipFile
@@ -11,7 +12,7 @@ from metashare.storage.models import StorageObject, MASTER, PROXY, INTERNAL, \
 def inventory(request):
     if settings.SYNC_NEEDS_AUTHENTICATION and not request.user.has_perm('storage.can_sync'):
         return HttpResponse("Forbidden: only synchronization users can access this page.", status=403)
-    
+
     # check for compatible sync protocol version
     sync_protocol = None
     if 'sync_protocol' in request.GET:
@@ -19,14 +20,14 @@ def inventory(request):
             if _prot in  settings.SYNC_PROTOCOLS:
                 sync_protocol = _prot
                 break
-    
+
     if not sync_protocol:
         # either no sync protocol parameter was send (which means the client is 
         # pre 3.0 and that synchronization protocol is no longer supported) or
         # no match was found between the client and server supported sync 
         # protocols
         return HttpResponse(status=501)
-    
+
     response = HttpResponse(status=200, content_type='application/zip')
     response['Metashare-Version'] = settings.METASHARE_VERSION
     response['Content-Disposition'] = 'attachment; filename="inventory.zip"'
@@ -49,9 +50,15 @@ def inventory(request):
 #            pass
     for obj in objects_to_sync:
         json_response[obj.identifier] = obj.get_digest_checksum()
-    
-    with ZipFile(response, 'w') as outzip:
+
+    # Create a StringIO object to make ZipFile write to response in python 2.7.9
+    response_stringio = StringIO(response)
+
+    with ZipFile(response_stringio, 'w') as outzip:
         outzip.writestr('inventory.json', json.dumps(json_response))
+
+    #write the StringIO contents to response an return it
+    response.write(response_stringio.getvalue())
     return response
 
 def full_metadata(request, resource_uuid):
@@ -78,4 +85,3 @@ def full_metadata(request, resource_uuid):
 #        outzip.writestr('storage-global.json', str(storage_object.identifier))
 #        outzip.writestr('metadata.xml', storage_object.metadata.encode('utf-8'))
     return response
-    
